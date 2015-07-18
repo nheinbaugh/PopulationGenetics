7d8506d51dcf581e2b290e613d28bd00c62df678
Nick Heinbaugh <nick.heinbaugh@outlook.com>
2015-07-17 23:20:38 -0700

    Rethinking the idea of what a gene is.... is it a representation of what can occur on a locus? or is it a gene in a person (which is what is at the locus...)

--- PopulationGenetics.World/Gene.cs
+++ PopulationGenetics.World/Gene.cs
@@ -4,23 +4,12 @@ namespace PopulationGenetics.Library
 {
     public interface IGene
     {
-        IAllele[] Alleles { get; }
     }
 
     public class Gene : IGene
     {
-        IAllele[] _alleles;
-        public IAllele[] Alleles
-        {
-            get { return _alleles; }
-        }
 
-        public Gene(IAllele firstAllele, IAllele secondAllele)
-        {
-
-        }
-
-        public Gene(IGene parentOne, IGene parentTwo)
+        public Gene()
         {
 
         }
