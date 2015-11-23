36ef2431394ca39e100681ff87dbf5104cbebf18
Nick Heinbaugh <nick.heinbaugh@live.com>
2015-11-22 17:12:44 -0800

    Who doesn't love manually moving pixels around? Now where are the damned images etc

--- PopulationGenetics.Client/MainWindow.xaml.cs
+++ PopulationGenetics.Client/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using PopulationGenetics.Library.Interfaces;
+using Fluent;
 
 namespace PopulationGenetics.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, IMainWindow
+    public partial class MainWindow : RibbonWindow, IMainWindow
     {
         private IWorld _world;
         public IWorld World => _world;
@@ -16,29 +17,36 @@ namespace PopulationGenetics.Client
         {
             _world = world;
             InitializeComponent();
-            CreateToolbar();
+            CreateRibbon();
             world.CreateWorldControls(geneGrid);
             world.GeneBank.UpdateVisibleControls(geneGrid);
 
         }
 
-        private void CreateToolbar()
+        private void CreateRibbon()
         {
-            
-            var tbt = new ToolBarTray();
-            var tb = new ToolBar();
-            tbt.ToolBars.Add(tb);
-            tbt.Height = 30;
-            var processTurn = new Button();
-            processTurn.Content = "Process Turn";
-            processTurn.Click += processTurn_Click;
-            var cleanWorld = new Button();
-            cleanWorld.Content = "Reset World";
-            cleanWorld.Click += cleanWorld_Click;
+            var ribbon = new Ribbon();
+            ribbon.MinHeight = 100;
+            var backstage = new Backstage();
+            ribbon.ToolBarItems.Add(backstage);
+           //mainGrid.Children.Add(ribbon);
 
-            tb.Items.Add(processTurn);
-            tb.Items.Add(cleanWorld);
-            toolbarGrid.Children.Add(tbt);
+
+            //var group = new RibbonGroupBox();
+            //group.Header = "This is a group box";
+
+            //var processTurn = new Fluent.Button();
+            //processTurn.Content = "Process Turn";
+            //processTurn.Click += processTurn_Click;
+            //var cleanWorld = new Fluent.Button();
+            //cleanWorld.Content = "Reset World";
+            //cleanWorld.Click += cleanWorld_Click;
+            //group.Items.Add(processTurn);
+            //group.Items.Add(cleanWorld);
+            //mainTab.Groups.Add(group);
+
+
+
         }
 
         private void cleanWorld_Click(object sender, RoutedEventArgs e)
